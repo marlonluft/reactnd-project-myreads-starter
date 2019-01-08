d872d7e3deb2dc579160b1d8b21c04e48c097aaa
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2019-01-07 21:11:10 -0300

    style: Identação App.js

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ import { Route } from 'react-router-dom'
 class BooksApp extends React.Component {
   render() {
     return (
-        <div className="app">
-          <Route exact path='/search' component={SearchBooks}/>
-          <Route exact path='/' component={ListBooks}/>
-        </div>
+      <div className="app">
+        <Route exact path='/search' component={SearchBooks} />
+        <Route exact path='/' component={ListBooks} />
+      </div>
     )
   }
 }
