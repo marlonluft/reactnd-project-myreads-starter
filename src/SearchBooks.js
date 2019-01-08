17ccb14c42f30ec92f9986444dca47c147c87c20
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2019-01-07 22:14:03 -0300

    feat: Consulta de livros

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -2,9 +2,43 @@ import React from 'react'
 import './App.css'
 import { Link } from 'react-router-dom'
 import BooksGrid from './BooksGrid.js'
+import * as BooksAPI from './BooksAPI'
 
 class SearchBooks extends React.Component {
+    state = {
+        search: '',
+        booksFounded: []
+    }
+
+    onSearchChange = (searchValue) => {
+
+        searchValue = searchValue.trim();
+
+        this.setState({
+            search: searchValue
+        })
+
+        this.ListBooks(searchValue);
+    }
+
+    ListBooks = (searchValue) => {
+        if (/\S/.test(searchValue)) {
+            BooksAPI.search(searchValue)
+                .then(books => this.setState({
+                    booksFounded: books
+                }))
+        }
+        else {
+            this.setState({
+                booksFounded: []
+            })
+        }
+    }
+
     render() {
+
+        const { search, booksFounded } = this.state
+
         return (
             <div className="search-books">
                 <div className="search-books-bar">
@@ -18,12 +52,12 @@ class SearchBooks extends React.Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                        <input type="text" placeholder="Search by title or author" />
+                        <input type="text" value={search} onChange={(event) => this.onSearchChange(event.target.value)} placeholder="Search by title or author" />
 
                     </div>
                 </div>
                 <div className="search-books-results">
-                    <BooksGrid books={[]} />
+                    <BooksGrid books={booksFounded} />
                 </div>
             </div>
         )
