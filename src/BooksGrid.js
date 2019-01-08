5da04664544bc89b00991b033b22eada3790ae6a
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2019-01-07 22:14:23 -0300

    feat: Exibição de dados de livros

--- src/BooksGrid.js
+++ src/BooksGrid.js
@@ -6,14 +6,22 @@ BooksGrid.propTypes = {
     books: propTypes.array.isRequired
 }
 
+function concatAuthors(authors) {
+    return authors ?
+    authors.join(', ') : '';
+}
+
 function BooksGrid(props) {
+
+
+
     return (
         <ol className="books-grid">
             {props.books.map((book) => (
-                <li>
+                <li key={book.id}>
                     <div className="book">
                         <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
                             <div className="book-shelf-changer">
                                 <select>
                                     <option value="move" disabled>Move to...</option>
@@ -24,8 +32,8 @@ function BooksGrid(props) {
                                 </select>
                             </div>
                         </div>
-                        <div className="book-title">To Kill a Mockingbird</div>
-                        <div className="book-authors">Harper Lee</div>
+                        <div className="book-title">{book.title}</div>
+                        <div className="book-authors">{concatAuthors(book.authors)}</div>
                     </div>
                 </li>
             ))}
