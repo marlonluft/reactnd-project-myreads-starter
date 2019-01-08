fc307412fa30976290bc9bbdef592cbe76804713
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2019-01-08 17:00:43 -0300

    feat: Listagem de livros conforme categoria escolhida

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -2,9 +2,36 @@ import React from 'react'
 import './App.css'
 import { Link } from 'react-router-dom'
 import BooksGrid from './BooksGrid.js'
+import * as BooksAPI from './BooksAPI'
 
 class ListBooks extends React.Component {
+    state = {
+        BooksCurrentlyReading: [],
+        BooksWantToRead: [],
+        BooksRead: []
+    }
+
+    componentDidMount() {
+        BooksAPI.getAll().then((books) => {
+
+            this.setState({
+                BooksCurrentlyReading: books.filter((book) => {
+                    return book.shelf === 'currentlyReading'
+                }),
+                BooksWantToRead: books.filter((book) => {
+                    return book.shelf === 'wantToRead'
+                }),
+                BooksRead: books.filter((book) => {
+                    return book.shelf === 'read'
+                })
+            })
+        })
+    }
+
     render() {
+
+        const { BooksCurrentlyReading, BooksWantToRead, BooksRead } = this.state
+
         return (
             <div className="list-books">
                 <div className="list-books-title">
@@ -15,19 +42,19 @@ class ListBooks extends React.Component {
                         <div className="bookshelf">
                             <h2 className="bookshelf-title">Currently Reading</h2>
                             <div className="bookshelf-books">
-                                <BooksGrid books={[]} />
+                                <BooksGrid books={BooksCurrentlyReading} />
                             </div>
                         </div>
                         <div className="bookshelf">
                             <h2 className="bookshelf-title">Want to Read</h2>
                             <div className="bookshelf-books">
-                                <BooksGrid books={[]} />
+                                <BooksGrid books={BooksWantToRead} />
                             </div>
                         </div>
                         <div className="bookshelf">
                             <h2 className="bookshelf-title">Read</h2>
                             <div className="bookshelf-books">
-                                <BooksGrid books={[]} />
+                                <BooksGrid books={BooksRead} />
                             </div>
                         </div>
                     </div>
